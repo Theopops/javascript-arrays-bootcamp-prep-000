9e70e006e8639bec036a8738816543d87faa76f6
Rachel Thackeray <rmthackeray@gmail.com>
2018-08-27 18:26:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,7 +4,8 @@ function addElementToBeginningOfArray(chocolateBars, marsBar) {
   return [marsBar, ...chocolateBars] 
 }
 function destructivelyAddElementToBeginningOfArray(chocolateBars, marsBar) {
-  return [marsBar, ...chocolateBars]
+    chocolateBars.unshift(marsBar);
+  return chocolateBars;
 }
 function addElementToEndOfArray(chocolateBars, marsBar) {
   return [...chocolateBars, marsBar] 
