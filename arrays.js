e2cc50c95dcac5d132348d545326ccd977dac43d
Rachel Thackeray <rmthackeray@gmail.com>
2018-08-27 15:57:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,12 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
-var marsBar = "foo"
+
 function addElementToBeginningOfArray(chocolateBars, marsBar) {
   return [marsBar, ...chocolateBars] 
 }
 function destructivelyAddElementToBeginningOfArray(chocolateBars, marsBar) {
   chocolateBars.shift(marsBar);
   return chocolateBars;
+}
+function addElementToEndOfArray(chocolateBars, marsBar) {
+  return [...chocolateBars, marsBar] 
 }
