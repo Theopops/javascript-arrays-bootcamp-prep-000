aa9ec222c4e6818031cda47372a3961677e410a8
Rachel Thackeray <rmthackeray@gmail.com>
2018-08-27 15:31:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
-function addElementToBeginningOfArray() {
+function addElementToBeginningOfArray(array, element) {
   return [marsBar, ...chocolateBars] 
 }
